4c23240a4c98a5b0d1d91fe5c499b990dbf36c36
GabrielToran <toranigabi@gmail.com>
2026-04-17 14:05:36 -0400

    Add files via upload

---
+++ AdminDashboardPage.xaml.cs
@@ -0,0 +1,49 @@
+using Multiplatoform_Project.Services;
+
+namespace Multiplatoform_Project.AdminPages;
+
+public partial class AdminDashboardPage : ContentPage
+{
+	public AdminDashboardPage()
+	{
+		InitializeComponent();
+	}
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await LoadStatsAsync();
+    }
+
+    private async Task LoadStatsAsync()
+    {
+        try
+        {
+            var products = await FirebaseAuthService.Instance.GetAllProductsAsync();
+            var categories = await FirebaseAuthService.Instance.GetCategoriesAsync();
+            var orders = await FirebaseAuthService.Instance.GetAllOrdersAsync();
+
+            ProductCountLabel.Text = products.Count.ToString();
+            CategoryCountLabel.Text = categories.Count.ToString();
+            OrderCountLabel.Text = orders.Count.ToString();
+        }
+        catch { /* silently fail on stats */ }
+    }
+
+    private async void OnProductsTapped(object sender, TappedEventArgs e)
+        => await Navigation.PushAsync(new AdminProductsPage());
+
+    private async void OnCategoriesTapped(object sender, TappedEventArgs e)
+        => await Navigation.PushAsync(new AdminCategoriesPage());
+
+    private async void OnOrdersTapped(object sender, TappedEventArgs e)
+        => await Navigation.PushAsync(new AdminOrdersPage());
+
+    private async void OnLogoutClicked(object sender, EventArgs e)
+    {
+        bool confirm = await DisplayAlert("Logout", "Are you sure you want to logout?", "Logout", "Cancel");
+        if (!confirm) return;
+
+        FirebaseAuthService.Instance.SignOut();
+        Application.Current!.MainPage = new NavigationPage(new LoginPage());
+    }
+}
