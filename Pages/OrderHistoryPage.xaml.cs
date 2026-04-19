79a49a076495c3a055a216099465994ac7aa2fd5
GabrielToran <toranigabi@gmail.com>
2026-04-18 23:30:26 -0400

    Add files via upload

---
+++ Pages/OrderHistoryPage.xaml.cs
@@ -0,0 +1,41 @@
+using Multiplatoform_Project.Services;
+using Multiplatoform_Project.Pages;
+
+namespace Multiplatoform_Project.Pages;
+
+public partial class OrderHistoryPage : ContentPage
+{
+	public OrderHistoryPage()
+	{
+		InitializeComponent();
+	}
+
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await LoadOrdersAsync();
+    }
+
+    private async Task LoadOrdersAsync()
+    {
+        LoadingIndicator.IsVisible = true;
+        try
+        {
+            string userId = FirebaseAuthServices.Instance.CurrentUserId ?? "";
+            var orders = await FirebaseAuthServices.Instance.GetUserOrdersAsync(userId);
+            OrdersCollection.ItemsSource = orders;
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"Could not load orders: {ex.Message}", "OK");
+        }
+        finally
+        {
+            LoadingIndicator.IsVisible = false;
+        }
+    }
+
+    private async void OnBackClicked(object sender, EventArgs e)
+        => await Navigation.PopAsync();
+}
